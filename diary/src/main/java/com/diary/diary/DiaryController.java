26bd4405ea9501e57197f9c32bc5a2a2076f9f3b
Love Rumar Karlquist <lovebandy@hotmail.se>
2023-12-15 10:26:52 +0100

    Har gjort så tillfällig data dyker upp på sidan och är synliga #17

--- diary/src/main/java/com/diary/diary/DiaryController.java
+++ diary/src/main/java/com/diary/diary/DiaryController.java
@@ -1,13 +1,31 @@
 package com.diary.diary;
 
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
 
 @Controller
 public class DiaryController {
+
+    @Autowired
+    private DiaryRepository diaryRepository;
     
     @GetMapping
-    public String getIndex() {
+    public String getIndex(Model model) {
+
+        model.addAttribute("diaries", diaryRepository.findAll());
+
         return "index";
+    }
+
+    @GetMapping("/new-diary")
+    public String addNew() {
+
+        Diary diary = new Diary();
+        diary.setTitle("Ny diary");
+        diaryRepository.save(diary);
+
+        return "redirect:/";
     }
 }

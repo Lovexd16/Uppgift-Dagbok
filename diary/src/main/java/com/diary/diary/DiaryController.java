fb29e41bfed5518e95bdaa2894ae5e98e4f8d6f6
Love Rumar Karlquist <lovebandy@hotmail.se>
2023-12-15 10:14:18 +0100

    Controller klass & index.html skapad #12, #15
    Även en mapping till index i Controller

---
+++ diary/src/main/java/com/diary/diary/DiaryController.java
@@ -0,0 +1,13 @@
+package com.diary.diary;
+
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.GetMapping;
+
+@Controller
+public class DiaryController {
+    
+    @GetMapping
+    public String getIndex() {
+        return "index";
+    }
+}

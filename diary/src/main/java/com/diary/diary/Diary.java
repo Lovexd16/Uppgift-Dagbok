535130412b463354b853cd63d49d835e13d10295
Love Rumar Karlquist <lovebandy@hotmail.se>
2023-12-15 10:05:52 +0100

    Har lagt till Diary.java klassen #10, #11
    Lade även till kolumnerna från databasen och getters + setters

---
+++ diary/src/main/java/com/diary/diary/Diary.java
@@ -0,0 +1,54 @@
+package com.diary.diary;
+
+import java.sql.Date;
+
+import jakarta.persistence.Entity;
+import jakarta.persistence.GeneratedValue;
+import jakarta.persistence.GenerationType;
+import jakarta.persistence.Id;
+
+@Entity
+public class Diary {
+    
+    private String title;
+    private String text;
+    private Date date;
+
+    @Id
+    @GeneratedValue(strategy = GenerationType.AUTO)
+    private int id;
+
+    public String getTitle() {
+        return title;
+    }
+
+    public void setTitle(String title) {
+        this.title = title;
+    }
+
+    public String getText() {
+        return text;
+    }
+
+    public void setText(String text) {
+        this.text = text;
+    }
+
+    public Date getDate() {
+        return date;
+    }
+
+    public void setDate(Date date) {
+        this.date = date;
+    }
+
+    public int getId() {
+        return id;
+    }
+
+    public void setId(int id) {
+        this.id = id;
+    }
+
+    
+}
